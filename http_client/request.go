cddb0a8d8be1c2d48922246109ea0d9892c20e86
drei <andredbenedetti@gmail.com>
2023-10-08 00:17:05 -0300

    parse headers

--- http_client/request.go
+++ http_client/request.go
@@ -7,13 +7,12 @@ import (
 	"io"
 	"log"
 	"net"
+	"strings"
 )
-
-type StatusCode struct {
-}
 
 type Response struct {
 	statusCode int
+	headers    map[string]string
 }
 
 func isValidHttpMethod(method string) bool {
@@ -29,6 +28,25 @@ func isValidHttpMethod(method string) bool {
 	// Out of curiosity, the code block above was benchmarked against the one below.
 	// The if chain is 30x faster in the best case (method == "GET"). The worst case also is roughly the same
 	return method == "GET" || method == "HEAD" || method == "POST" || method == "PUT" || method == "DELETE" || method == "CONNECT" || method == "OPTIONS" || method == "TRACE" || method == "PATCH"
+}
+
+func parseHeaders(raw []string) map[string]string {
+	headers := make(map[string]string, len(raw))
+	for _, val := range raw {
+		kv := strings.Split(val, ":")
+		if len(kv) < 2 {
+			log.Fatalf("Received malformed header %s", val)
+		}
+
+		key := strings.ToLower(kv[0])
+
+		v := strings.Split(kv[1], "\r\n")
+		value := strings.Trim(v[0], " ")
+
+		headers[key] = value
+	}
+
+	return headers
 }
 
 func Request(method string, url string) (Response, error) {
@@ -69,12 +87,15 @@ func Request(method string, url string) (Response, error) {
 	resp.statusCode = statusCode
 
 	// Read headers
+	rawHeaders := make([]string, 0, 32)
 	for {
 		line, err := reader.ReadString('\n')
 		// End of headers is marked by a \r\n line
 		if line == "\r\n" {
 			break
 		}
+
+		rawHeaders = append(rawHeaders, line)
 
 		if err != nil {
 			// io.EOF should never be wrapped, so we don't need to use errors.Is,
@@ -87,6 +108,8 @@ func Request(method string, url string) (Response, error) {
 		}
 
 	}
+
+	resp.headers = parseHeaders(rawHeaders)
 
 	return resp, nil
 }

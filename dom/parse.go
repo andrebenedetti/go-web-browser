fbe8628adbfeb39a92e3f35d08262f32b28dbf2e
drei <andredbenedetti@gmail.com>
2023-10-16 19:22:09 -0300

    fix parsing by adding self-closing tags

--- dom/parse.go
+++ dom/parse.go
@@ -2,6 +2,7 @@ package dom
 
 import (
 	"fmt"
+	"strings"
 )
 
 // elementType may be "Element" or "Text"
@@ -32,10 +33,20 @@ func (t *tree) AddTag(tag string) {
 	if tag == "!doctype html" {
 		return
 	}
-	if tag[0] == '/' {
+	if tag[0] == '/' || tag[len(tag)-1] == '/' {
 		fmt.Printf("Adding tag: %s\n", tag)
 		// pop last node
-		node := t.unfinished[len(t.unfinished)-1]
+
+		var node *Element
+		// self closing tag needs further parsing
+		if tag[len(tag)-1] == '/' {
+			tag = strings.Split(tag, " ")[0]
+			parent := t.unfinished[len(t.unfinished)-1]
+			node = &Element{text: tag, parent: parent, children: make([]*Element, 0, 1)}
+		} else {
+			node = t.unfinished[len(t.unfinished)-1]
+		}
+
 		if len(t.unfinished) == 1 {
 			t.Root = node
 			return
@@ -59,63 +70,6 @@ func (t *tree) AddTag(tag string) {
 	}
 }
 
-// func (t *tree) Finish() *Element {
-// 	fmt.Println("Finishing")
-// 	fmt.Println(t.unfinished)
-// 	fmt.Println(len(t.unfinished))
-// 	if len(t.unfinished) == 0 {
-// 		t.AddTag("html")
-// 	}
-// 	for len(t.unfinished) > 1 {
-// 		node := t.unfinished[len(t.unfinished)-1]
-// 		t.unfinished = t.unfinished[:len(t.unfinished)-1]
-// 		parent := t.unfinished[len(t.unfinished)-1]
-// 		parent.children = append(parent.children, node)
-// 	}
-
-// 	res := t.unfinished[len(t.unfinished)-1]
-// 	t.unfinished = t.unfinished[:len(t.unfinished)-1]
-// 	return res
-// }
-
-// func findHtmlTag(document string) (int, error) {
-// 	inTag := false
-// 	tag := ""
-// 	for i, c := range document {
-
-// 		if c == '<' {
-// 			inTag = true
-// 		} else if c == '>' {
-// 			inTag = false
-// 			if tag == "body" {
-// 				return i - 5, nil
-// 			} else {
-// 				tag = ""
-// 			}
-// 		} else if inTag {
-// 			tag += string(c)
-// 		}
-// 	}
-
-// 	return -1, errors.New("tag <html> was not found in document")
-// }
-
-// Parse picks up a complete document, which might include
-// <head>, <script>, <css> and other things, separates the html
-// body and builds the DOM tree with it.
-// func Parse(document string, tree *tree) error {
-// 	// var htmlBody string
-
-// 	// htmlTagIndex, err := findHtmlTag(document)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	fmt.Println(document[htmlTagIndex:])
-// 	parseBody(document[htmlTagIndex:], tree)
-
-// 	return nil
-// }
-
 func PrintTree(root *Element, indent int) {
 	if root == nil {
 		return
@@ -130,8 +84,6 @@ func PrintTree(root *Element, indent int) {
 	}
 }
 
-// TODO: we need a "finish" function to handle the case when we have text (unclosed, of course)
-// together with the finishing tag ("html")
 func ParseBody(body string, tree *tree) {
 	text := ""
 	inTag := false

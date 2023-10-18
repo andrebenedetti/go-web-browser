4e6681f31b74c6c18275ea320824b414d3cce475
drei <andredbenedetti@gmail.com>
2023-10-18 20:11:30 -0300

    new UI library

--- browser/browser.go
+++ browser/browser.go
@@ -1,59 +1,23 @@
 package browser
 
 import (
-	"image/color"
-
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/text"
-	"golang.org/x/image/font"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 )
 
-type Browser struct {
-	Document string
-	Font     font.Face
+func onSubmitAddressBar() {
 }
 
-func (b *Browser) Update() error {
-	return nil
+func Run() {
+	a := app.New()
+	window := a.NewWindow("Go Web Browser")
+	entry := widget.NewEntry()
+
+	form := &widget.Form{
+		Items: []*widget.FormItem{
+			{Text: "Go to:", Widget: entry}},
+		OnSubmit: onSubmitAddressBar,
+	}
+	window.SetContent(form)
+	window.ShowAndRun()
 }
-
-func (b *Browser) Draw(screen *ebiten.Image) {
-
-	text.Draw(screen, b.Document, b.Font, 0, 0, color.White)
-}
-
-func (b *Browser) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return outsideWidth, outsideHeight
-}
-
-// func Start() {
-// 	// Initial tab
-// 	url := "example.org"
-// 	document, err := http_client.RetrieveUrl(url)
-// 	if err != nil {
-// 		log.Fatalf("Error visting %s: %s", url, err.Error())
-// 	}
-
-// 	// Load font
-// 	tt, err := opentype.Parse(fonts.MPlus1pRegular_ttf)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	const dpi = 72
-// 	mplusNormalFont, err := opentype.NewFace(tt, &opentype.FaceOptions{
-// 		Size:    24,
-// 		DPI:     dpi,
-// 		Hinting: font.HintingVertical,
-// 	})
-
-// 	if err != nil {
-// 		log.Fatal("Failed to initialize fonts")
-// 	}
-
-// 	ebiten.SetWindowTitle("Go Web Browser")
-// 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
-
-// 	if err := ebiten.RunGame(&Browser{Document: dom.ParseText(document.Body), Font: mplusNormalFont}); err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
--- main.go
+++ main.go
@@ -1,36 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"strings"
-
-	"github.com/andrebenedetti/go-web-browser/dom"
-	"github.com/andrebenedetti/go-web-browser/http_client"
-)
+import "github.com/andrebenedetti/go-web-browser/browser"
 
 func main() {
-	// browser.Start()
-
-	// tree := dom.NewTree()
-	// tree.AddTag("html")
-	// tree.AddTag("p")
-	// tree.AddText("Lorem ipsum")
-	// tree.AddTag("/p")
-	// tree.AddTag("/html")
-	// dom.PrintTree(tree.Root, 0)
-	tree := dom.NewTree()
-	fmt.Println(tree)
-
-	res, err := http_client.RetrieveUrl("example.org")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	body := strings.ReplaceAll(res.Body, "\n", "")
-	fmt.Println(body)
-	dom.ParseBody(body, tree)
-
-	dom.PrintTree(tree.Root, 0)
-
+	browser.Run()
 }
